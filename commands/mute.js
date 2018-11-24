0371eec4cdb2b52a290cb3b44b408ffc059613e1
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-11-23 21:40:30 -0800

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -6,8 +6,8 @@ module.exports.run = async (bot, message, args) => {
   //Checking to make sure everything works
   let tomute = message.mentions.members.first() || message.guild.members.get(args[0]);
   if(!tomute) return message.reply("Couldn't find user.");
-  if(!message.member.roles.some(r=>["Founder", "Head Moderator", "Moderator", "Server Owner", "Admin", "Trial Moderator"].includes(r.name)) )
-  return message.reply("Sorry, you don't have permission to use this! (Actually I'm not sorry please use \`+report\` if the user is breaking a rule thanks).");
+  if(!message.member.roles.some(r=>["Police"].includes(r.name)) )
+  return message.reply("Sorry, you don't have permission to use this!");
   let muterole = message.guild.roles.find(`name`, "Muted");
   if(!muterole) return message.reply("You have no muted role, come back after you've made it and try again.");
   let mutetime = args[1];
@@ -18,12 +18,12 @@ module.exports.run = async (bot, message, args) => {
   //The punishment bit!
   await(tomute.addRole(muterole.id));
   message.reply(`<@${tomute.id}> was muted!`);
-  tomute.send(`You've been muted in one of Kessler's servers by ${message.author.tag} for ${mutetime} because of ${reason}. I'll DM you when you get unmuted.`);
+  tomute.send(`You've been muted in GraxCounty by ${message.author.tag} for ${mutetime} because of ${reason}. I'll DM you when you get unmuted.`);
 
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
-    tomute.send("You've been unmuted in Kessler's server, don't screw up again!");
+    tomute.send("You've been unmuted in GraxCounty, don't screw up again!");
 
 
   }, ms(mutetime));
